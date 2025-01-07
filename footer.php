39bed6003e79d1b623d7dcfe56cfbe40c49d6789
vijindal <vijindal@gmail.com>
2025-01-07 13:35:49 +0530

    update

---
+++ footer.php
@@ -0,0 +1,18 @@
+<footer class="bg-dark text-white py-4 mt-auto">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <h5>Address:</h5>
+                <p>IIT Campus, (BHU) Varanasi, Uttar Pradesh (India), 221005</p>
+            </div>
+            <div class="col-md-4">
+                <h5>Phone No:</h5>
+                <p>+91-542-7165640</p>
+            </div>
+            <div class="col-md-4">
+                <h5>Email:</h5>
+                <p><a href="mailto:vjindal.met@iitbhu.ac.in" class="text-white">vjindal.met@iitbhu.ac.in</a></p>
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ header.php
@@ -0,0 +1,20 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="index.html">
+        <img src="image/clg_logo.jpg" alt="IIT BHU Logo" style="height: 50px;">
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item"><a class="nav-link" href="index.html">Home</a></li>
+            <li class="nav-item"><a class="nav-link" href="Research.html">Research</a></li>
+            <li class="nav-item"><a class="nav-link" href="Publications.html">Publications</a></li>
+            <li class="nav-item"><a class="nav-link" href="Lab_facilities.html">Lab Facilities</a></li>
+            <li class="nav-item active"><a class="nav-link" href="Teaching.html">Teaching</a></li>
+            <li class="nav-item"><a class="nav-link" href="People.html">People</a></li>
+            <li class="nav-item"><a class="nav-link" href="Openings.html">Openings</a></li>
+            <li class="nav-item"><a class="nav-link" href="Contact_Us.html">Contact</a></li>
+        </ul>
+    </div>
+</nav>
